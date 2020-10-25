df5ec14f61a3cf1512ef955b0859a7fcbbbb4436
luapbbor <luapbbor@gmail.com>
2020-10-26 07:08:28 +1100

    Added javascript for keyboard input

--- inc/Game.php
+++ inc/Game.php
@@ -7,125 +7,134 @@ class Game {
     // Used to set how many wrong guesses a player has before game over.
     public $lives = 5;
 
-    function __construct(Phrase $phrase) {
-       $this->phrase = $phrase;
-    }
-
-    public function keyHandler($letter) {
-      
-      if(!in_array($letter,$this->phrase->selected)) {
-             return '<button type="submit" class="key" name="key" id="'.$letter.'"value="'.$letter.'">'.$letter.'</button>';
-      } else {
-        if($this->phrase->checkLetter($letter)) {
-          
-          return '<button type="submit" class="key correct" name="key" value="'.$letter.'" disabled>'.$letter.'</button>';
-        } else {
-          return '<button type="submit" class="key incorrect" name="key" value="'.$letter.'" disabled>'.$letter.'</button>';
-
-        }
-        
-      } 
-        // return '<button class="key" type="submit" name="key" value="q">q</button>';
-      
-      
-    }
-
-    public function displayKeyboard(){
+function __construct(Phrase $phrase) {
+    $this->phrase = $phrase;
+}
     
-      $topRow = ['q', 'w', 'e','r','t','y','u','i','o','p'];
-      $middleRow = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
-      $bottomRow = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
-    $keyboardHTML = "";
-    $keyboardHTML .= '<div class="keyrow">';
-    foreach($topRow as $letter){
-      $keyboardHTML .= $this->keyHandler($letter);
-    }
-    $keyboardHTML .= '</div>';
-
-    $keyboardHTML .= '<div class="keyrow">';
-    foreach($middleRow as $letter){
-      $keyboardHTML .= $this->keyHandler($letter);
-    }
-    $keyboardHTML .= '</div>';
-
-    $keyboardHTML .= '<div class="keyrow">';
-    foreach($bottomRow as $letter){
-      $keyboardHTML .= $this->keyHandler($letter);
-    }
-    $keyboardHTML .= '</div>';
-        
-    return $keyboardHTML;
+// This function handles if a key on the onboard keyboard has been pressed
+public function keyHandler($letter) {
+  
+  // if the key is not a key that has already been pressed
+  if(!in_array($letter,$this->phrase->selected)) {
+          return '<button type="submit" class="key" name="key" id="'.$letter.'"value="'.$letter.'">'.$letter.'</button>';
+  } else {
+    if($this->phrase->checkLetter($letter)) {
+      // If  they key pressed was in the current phrase, adde the "correct" class
+      return '<button type="submit" class="key correct" name="key" value="'.$letter.'" disabled>'.$letter.'</button>';
+    } else {
+      // Else, add the "incorrect" class
+      return '<button type="submit" class="key incorrect" name="key" value="'.$letter.'" disabled>'.$letter.'</button>';
 
     }
+    
+  } 
 
-    // This function displays the score on the screen
-    public function displayScore() {
-    $scoreHTML = "";
-    // total number of lives at start of game
-    $lives = $this->lives;
-    // Livesleft decreases with each incorrect letter guess
-    $livesLeft = $lives - $this->phrase->numberLost();
-       
-    // Loop through livesleft and show the liveheart html
-    for ($j = 1; $j<=$livesLeft;$j+=1 ) {
-          $scoreHTML .= '<li class="tries"><img src="images/liveHeart.png" height="35px" widght="30px"></li>';
-    }
+}
      
-    // Loop through the number of incorrect guesses and show the lostheart html
-    for($i = 1; $i<=$this->phrase->numberLost(); $i+=1) {
-        $scoreHTML .= '<li class="tries"><img src="images/lostHeart.png" height="35px" widght="30px"></li>';        
-    }
+// This function displays the keys on the screen
+public function displayKeyboard(){
 
-    echo $scoreHTML;
-    }
+  $topRow = ['q', 'w', 'e','r','t','y','u','i','o','p'];
+  $middleRow = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+  $bottomRow = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
-    public function checkForLose() {
-      if ($this->phrase->numberLost() >= $this->lives) {
-        return true;
-      } else {
-        return false;
-      }
-    }
+  $keyboardHTML = "";
+  $keyboardHTML .= '<div class="keyrow">';
+  foreach($topRow as $letter){
+    $keyboardHTML .= $this->keyHandler($letter);
+  }
+  $keyboardHTML .= '</div>';
 
-    public function checkForWin() {
-      $resultArray = array_intersect($this->phrase->selected,$this->phrase->getLetterArray());
-      if(count($resultArray) == count($this->phrase->getLetterArray())) {
-         return true;
-      } else  {
-         return false;
-      }
-    }
+  $keyboardHTML .= '<div class="keyrow">';
+  foreach($middleRow as $letter){
+    $keyboardHTML .= $this->keyHandler($letter);
+  }
+  $keyboardHTML .= '</div>';
 
-    public function gameOver() {
-      $gameOverHTML = "";
-      if ($this->checkForLose()) {   
-        $gameOverHTML .= '<div id="lose-overlay">';  
-        $gameOverHTML .= '<div id="banner" class="section">';
-        $gameOverHTML .= '<h2 class="header">Phrase Hunter</h2>'; 
-        $gameOverHTML .= '</div>'; 
-        $gameOverHTML .= '<h1 id="game-over-message">The phrase was: ' . $this->phrase->currentPhrase . '. Better luck next time!</h1>'; 
-        $gameOverHTML .= '<form action="play.php" method="post">';
-        $gameOverHTML .= '<input id="btn__reset" type="submit" name="start" value="Re-start Game" />';
-        $gameOverHTML .= '</form>';
-        $gameOverHTML .= '</div>';       
-        return $gameOverHTML;
-      } elseif ($this->checkForWin()) {
-        $gameOverHTML .= '<div id="win-overlay">';  
-        $gameOverHTML .= '<div id="banner" class="section">';
-        $gameOverHTML .= '<h2 class="header">Phrase Hunter</h2>'; 
-        $gameOverHTML .= '</div>';  
-        $gameOverHTML .= '<h1 id="game-over-message">Congratualations on guessing the phrase: ' . $this->phrase->currentPhrase . '</h1>'; 
-        $gameOverHTML .= '<form action="play.php" method="post">';
-        $gameOverHTML .= '<input id="btn__reset" type="submit" name="start" value="Re-start Game" />';
-        $gameOverHTML .= '</form>';
-        $gameOverHTML .= '</div>';   
-        return $gameOverHTML;
+  $keyboardHTML .= '<div class="keyrow">';
+  foreach($bottomRow as $letter){
+    $keyboardHTML .= $this->keyHandler($letter);
+  }
+  $keyboardHTML .= '</div>';
+      
+  return $keyboardHTML;
+}
 
-      } else {
-        return false;
-      }
-    }
+
+// This function displays the score on the screen
+public function displayScore() {
+$scoreHTML = "";
+// total number of lives at start of game
+$lives = $this->lives;
+// Livesleft decreases with each incorrect letter guess
+$livesLeft = $lives - $this->phrase->numberLost();
+    
+// Loop through livesleft and show the liveheart html
+for ($j = 1; $j<=$livesLeft;$j+=1 ) {
+      $scoreHTML .= '<li class="tries"><img src="images/liveHeart.png" height="35px" widght="30px"></li>';
+}
+  
+// Loop through the number of incorrect guesses and show the lostheart html
+for($i = 1; $i<=$this->phrase->numberLost(); $i+=1) {
+    $scoreHTML .= '<li class="tries"><img src="images/lostHeart.png" height="35px" widght="30px"></li>';        
+}
+
+echo $scoreHTML;
+}
+
+// This function checks if the game has been lost
+public function checkForLose() {
+  if ($this->phrase->numberLost() >= $this->lives) {
+    return true;
+  } else {
+    return false;
+  }
+}
+     
+// This function checks if the game has been won
+public function checkForWin() {
+  // array_intersect compares the values of two or more arrays and returns the matches
+  // Compare the selected array to the getletterarray
+  $resultArray = array_intersect($this->phrase->selected,$this->phrase->getLetterArray());
+  // If the number of items in the return array matches the get letter array
+  if(count($resultArray) == count($this->phrase->getLetterArray())) {
+      return true;
+  } else  {
+      return false;
+  }
+}
+   
+//This function generates the HTML based on if checkforlose or checkforwin are true or false
+public function gameOver() {
+  $gameOverHTML = "";
+  if ($this->checkForLose()) {   
+    $gameOverHTML .= '<div id="lose-overlay">';  
+    $gameOverHTML .= '<div id="banner" class="section">';
+    $gameOverHTML .= '<h2 class="header">Phrase Hunter</h2>'; 
+    $gameOverHTML .= '</div>'; 
+    $gameOverHTML .= '<h1 id="game-over-message">The phrase was: ' . $this->phrase->currentPhrase . '. Better luck next time!</h1>'; 
+    $gameOverHTML .= '<form action="play.php" method="post">';
+    $gameOverHTML .= '<input id="btn__reset" type="submit" name="start" class="start" value="Re-start Game" />';
+    $gameOverHTML .= '</form>';
+    $gameOverHTML .= '</div>';       
+    return $gameOverHTML;
+  } elseif ($this->checkForWin()) {
+    $gameOverHTML .= '<div id="win-overlay">';  
+    $gameOverHTML .= '<div id="banner" class="section">';
+    $gameOverHTML .= '<h2 class="header">Phrase Hunter</h2>'; 
+    $gameOverHTML .= '</div>';  
+    $gameOverHTML .= '<h1 id="game-over-message">Congratualations on guessing the phrase: ' . $this->phrase->currentPhrase . '</h1>'; 
+    $gameOverHTML .= '<form action="play.php" method="post">';
+    $gameOverHTML .= '<input id="btn__reset" type="submit" name="start" class="start" value="Re-start Game" />';
+    $gameOverHTML .= '</form>';
+    $gameOverHTML .= '</div>';   
+    return $gameOverHTML;
+
+  } else {
+    return false;
+  }
+}
   
     
   }  
--- inc/Phrase.php
+++ inc/Phrase.php
@@ -1,69 +1,72 @@
 <?php
 
 class Phrase {
-    // A string containing the current phrase to be used in the game
-    public $currentPhrase;
-    // An array of letters the user has guessed
-    public $selected = [];
+// A string containing the current phrase to be used in the game
+public $currentPhrase;
+// An array of letters the user has guessed
+public $selected = [];
+// Random phrases to select generate
+public $phrases = ['Boldness my friend', 'Leave no stone unturned', 'Broken crayons still colour', 'The adventure begins', 'love without limites'];
 
-    public $phrases = ['Boldness my friend', 'Leave no stone unturned', 'Broken crayons still colour', 'The adventure begins', 'love without limites'];
+function __construct($phrase = null, $selected = []) {
 
-    function __construct($phrase = null, $selected = []) {
-
-        if (!empty($phrase)) {
-            $this->currentPhrase = $phrase;
-            } else {
-             $arrayKey = array_rand($this->phrases,1);  
-             $this->currentPhrase = $this->phrases[$arrayKey];
-            }
-       
-        if (!empty($selected)) {
-            $this->selected = $selected;            
+    if (!empty($phrase)) {
+        $this->currentPhrase = $phrase;
+        } else {
+            $arrayKey = array_rand($this->phrases,1);  
+            $this->currentPhrase = $this->phrases[$arrayKey];
         }
-
+    
+    if (!empty($selected)) {
+        $this->selected = $selected;            
     }
 
+}
+
     
-    // This function formats and displays the current phrase
-    public function addPhraseToDisplay() {
-        $currentPhraseLower = strtolower($this->currentPhrase);
-        $letters = str_split($currentPhraseLower);
-       
-        foreach($letters as $letter){
-            
-            echo "<ul>";
-            if ($letter == " "){
-                echo "<li class='hide space'></li>";
-
+// This function formats and displays the current phrase
+public function addPhraseToDisplay() {
+    // puts the phrase to lowercase
+    $currentPhraseLower = strtolower($this->currentPhrase);
+    // Puts each letter from currentphase into an array
+    $letters = str_split($currentPhraseLower);
+    // Loop through each of those letters 
+    foreach($letters as $letter){
+        
+        echo "<ul>";
+        // If the letter is empty, add the space class
+        if ($letter == " "){
+            echo "<li class='hide space'></li>";
+        } else {
+            // If the letter is in the selected array add the show class, else add the hide class
+            if(in_array($letter,$this->selected)) {
+                echo "<li class='show letter " . $letter . "'>" . $letter . "</li>"; 
             } else {
-                if(in_array($letter,$this->selected)) {
-                    echo "<li class='show letter " . $letter . "'>" . $letter . "</li>"; 
-                } else {
-                 echo "<li class='hide letter " . $letter . "'>" . $letter . "</li>";
-                }
+                echo "<li class='hide letter " . $letter . "'>" . $letter . "</li>";
             }
-            echo "</ul>";
         }
-
+        echo "</ul>";
     }
 
+}
 
-    // This function will return the unique letters from $currentPhrase as an array  
-     
-    public function getLetterArray() {
-        //array of unique lowercase letters only in the currentPhrase
-  $uniqueLetters = array_unique(str_split(str_replace(
-      ' ',
-      '',
-      strtolower($this->currentPhrase)
-      )));
 
-  return $uniqueLetters;
-  }
+// This function will return the unique letters from $currentPhrase as an array  
+
+public function getLetterArray() {
+    //array of unique lowercase letters only in the currentPhrase
+    $uniqueLetters = array_unique(str_split(str_replace(
+    ' ',
+    '',
+    strtolower($this->currentPhrase)
+)));
+
+    return $uniqueLetters;
+}
      
-    // Checks to see if a specific letter is in the current phrase and returns true or false
-    public function checkLetter($letter) {
-    
+// Checks to see if a specific letter is in the current phrase and returns true or false
+public function checkLetter($letter) {
+
     $uniqueLetters = $this->getLetterArray();
     // var_dump($uniqueLetters);
     // var_dump($letter);
@@ -73,14 +76,14 @@ class Phrase {
 
     return false;
     }
-    }
+}
 
-    //This function compares the selected letters to the unique letters in currentphrase
-    // It puts the difference in an array and then the array is counted
-    public function numberLost(){
-        $difference = array_diff($this->selected,$this->getLetterArray());
-        return count($difference);
-    }
+//This function compares the selected letters to the unique letters in currentphrase
+// It puts the difference in an array and then the array is counted
+public function numberLost(){
+    $difference = array_diff($this->selected,$this->getLetterArray());
+    return count($difference);
+}
 
 }
 
